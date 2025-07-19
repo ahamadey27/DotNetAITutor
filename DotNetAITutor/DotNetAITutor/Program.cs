365361f92acb6666865f489825179558ff4d3086
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-07-18 20:33:24 -0400

    Add EvenNumbers functionality to Program.cs
    Implemented a method to find and display even numbers from a predefined list of integers. Imported the `DotNetAITutor` namespace and utilized the `EvenNumbers` method from the `CodingExercise01` class, outputting results as a comma-separated string.

--- DotNetAITutor/DotNetAITutor/Program.cs
+++ DotNetAITutor/DotNetAITutor/Program.cs
@@ -1,4 +1,7 @@
 ﻿using DotNetAITutor;
 
+//EvenNumbers()
 var input = new List<int>() { 2, 6, 78, 45, 3, 76, 7, 8 };
 var evens = CodingExercise01.EvenNumbers(input);
+// string.Join creates a single string by concatenating the elements of a collection, separated by a specified delimiter.
+Console.WriteLine(string.Join(", ", evens));
