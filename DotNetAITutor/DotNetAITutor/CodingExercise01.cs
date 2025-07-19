20cef26510b49c221ad1651c451ccad130ae7454
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-07-18 20:31:22 -0400

    first exercise / debugging

---
+++ DotNetAITutor/DotNetAITutor/CodingExercise01.cs
@@ -0,0 +1,11 @@
+﻿namespace DotNetAITutor
+{
+    public class CodingExercise01
+    {
+        //simple C# method that takes a list of integers and returns only the even numbers from that list.
+        public static List<int> EvenNumbers(List<int> numbers)
+        {
+            return numbers.Where(num => num % 2 == 0).ToList();
+        }
+    }
+}
---
+++ DotNetAITutor/DotNetAITutor/Program.cs
@@ -0,0 +1,4 @@
+﻿using DotNetAITutor;
+
+var input = new List<int>() { 2, 6, 78, 45, 3, 76, 7, 8 };
+var evens = CodingExercise01.EvenNumbers(input);
